9166b783e41b9fdc47f642e45b284c9cf9231508
Tuca <sirzarybr@hotmail.com>
2025-11-04 16:11:33 -0300

    ajusted a build to open in a .bat

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,21 +10,28 @@ repositories {
 }
 
 dependencies {
-    // MySQL
     implementation("mysql:mysql-connector-java:8.0.33")
 
-    // Lombok
     compileOnly("org.projectlombok:lombok:1.18.42")
     annotationProcessor("org.projectlombok:lombok:1.18.42")
 
-    // Testes
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-
-
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.tuca.Main"
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+    from({
+        configurations.runtimeClasspath.get().filter { it.exists() }.map { zipTree(it) }
+    })
 }
